a204e013605f946e7314e199430618d5a247d827
Joern Kohlmetz <joern.kohlmetz@gmx.de>
2016-11-05 20:18:27 +0000

    Initial file versions

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * http://usejsdoc.org/
+ */
+var express = require('express')
+, app = express()
+, server = require('http').createServer(app)
+, io = require('socket').listen(server)
+, conf = require('./config.json');
+
+server.listen(conf.port);
+
+app.configure(function(){
+	// statische Dateien ausliefern
+	app.use(express.static(__dirname + '/public'));
+});
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+	// so wird die Datei index.html ausgegeben
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+		// der Client ist verbunden
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!'} );
+	// wenn ein Benutzer einen Text sendet
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		io.socket.emit('chat', {zeit: new Date(), name: data.name || 'Anonym', text: data.text});
+		});
+	});
+
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
+
